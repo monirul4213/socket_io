b4515aeadac8f8053110f2fb0f1feaf9bcf8a470
monirul4213 <monirul4213@gmail.com>
2023-11-30 02:04:18 +0600

    file deleted

--- node js/app.js
+++ node js/app.js
@@ -12,21 +12,11 @@ var clients = {};
 io.on("connection", (socket) => {
   console.log("connetetd");
   console.log(socket.id, "has joined");
-  socket.on("signin", (id) => {
-    console.log(id);
-    clients[id] = socket;
-    console.log(clients);
-  });
+ 
 
   socket.on('message', (msg) => {
     console.log('message: ' + msg);
     io.emit('message', msg);
-  });
-
-  socket.on("message", (msg) => {
-    console.log(msg);
-    let targetId = msg.targetId;
-    if (clients[targetId]) clients[targetId].emit("message", msg);
   });
 
 
